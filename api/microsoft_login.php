0f1260f8fc20d162a55e637da351797f1284b6ee
roivincent <126530503+roicia@users.noreply.github.com>
2025-06-25 16:39:56 +0800

    added env

--- api/microsoft_login.php
+++ api/microsoft_login.php
@@ -2,18 +2,19 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // Load environment variables
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+// $dotenv->load();
 
-$clientId = $_ENV['AZURE_APP_ID'];
-$redirectUri = $_ENV['AZURE_REDIRECT_URI'];
-$tenantId = $_ENV['AZURE_TENANT_ID'];
-$scopes = explode(' ', $_ENV['AZURE_GRAPH_SCOPES']); // Convert space-separated string to array
+$clientId = getenv['AZURE_APP_ID'];
+$clientSecret = getenv['AZURE_CLIENT_SECRET'];
+$redirectUri = getenv['AZURE_REDIRECT_URI'];
+$tenantId = getenv['AZURE_TENANT_ID'];
+$scopes = explode(' ', getenv['AZURE_GRAPH_SCOPES']); // Convert space-separated string to array
 
 // Create the OAuth provider
 $provider = new \League\OAuth2\Client\Provider\GenericProvider([
     'clientId'                => $clientId,
-    'clientSecret'            => $_ENV['AZURE_CLIENT_SECRET'],
+    'clientSecret'            => $clientSecret,
     'redirectUri'             => $redirectUri,
     'urlAuthorize'            => "https://login.microsoftonline.com/{$tenantId}/oauth2/v2.0/authorize",
     'urlAccessToken'          => "https://login.microsoftonline.com/{$tenantId}/oauth2/v2.0/token",
