3a9ae5cf5b33d242700510d05ba0f2856141e0f7
roivincent <126530503+roicia@users.noreply.github.com>
2025-07-15 11:18:56 +0800

    added

--- config/config.php
+++ config/config.php
@@ -1,30 +1,16 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';  // Autoload Composer dependencies
-require __DIR__ . '/../constants.php'; // Assumed to contain DB_DRIVER, DB_HOST, DB_NAME, DB_USER, DB_PASS
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../constants.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
-use Illuminate\Events\Dispatcher; // Needed if you use event dispatcher
-use Illuminate\Container\Container; // Needed if you use event dispatcher
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
 
 date_default_timezone_set('Asia/Manila');
 
 $capsule = new Capsule;
-$capsule->addConnection([
-    'driver'    => DB_DRIVER,
-    'host'      => DB_HOST,
-    'database'  => DB_NAME,
-    'username'  => DB_USER,
-    'password'  => DB_PASS,
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-    'port'      => DB_PORT,
-    'sslmode'   => SSL_MODE,
-    'options' => extension_loaded('pdo_mysql') ? array_filter([
-        PDO::MYSQL_ATTR_SSL_CA => MYSQL_ATTR_SSL_CA,
-    ]) : [],
-]);
+$capsule->addConnection(CONFIG);
 
 // Optional: Set the event dispatcher if your models use events
 $capsule->setEventDispatcher(new Dispatcher(new Container));
@@ -35,5 +21,3 @@ $capsule->bootEloquent();
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-
-// Rest of your application code...
--- constants.php
+++ constants.php
@@ -1,47 +1,79 @@
 <?php
-define('ROOT',  $_SERVER['DOCUMENT_ROOT'] . '/');
+
+define('ROOT',  __DIR__);
 
 define('AZURE_APP_ID', 'abfed74c-1e74-4e39-972b-3fe0e9568db3');
 define('AZURE_CLIENT_SECRET', 'jCt8Q~KDn_AvAitZekzlzK_qg472kC5EuXxnUbQ-');
+define('AZURE_REDIRECT_URI', 'https://imus-it-ticket-system.azurewebsites.net/api/callback.php');
 define('AZURE_TENANT_ID', 'a8411b96-92c2-4538-9eba-9dcc50ef1162');
 define('AZURE_GRAPH_SCOPES', 'https://graph.microsoft.com/.default');
 
-$http = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
-define('API', $http . $_SERVER['HTTP_HOST'] . '/api/');
-define('API_KEY', 'joshbakla');
 
-define('SITE_URL', $http . $_SERVER['HTTP_HOST'] . '/');
-define('AZURE_REDIRECT_URI', $http . $_SERVER['HTTP_HOST'] . '/api/callback.php');
 
-function defaultDatabaseConfig($default_db = 'localhost')
+
+
+
+
+
+
+
+function defaultApiConfig($default_api = 'local')
 {
-    if ($default_db == 'azure') {
+    if ($default_api == 'azure') {
         return [
-            // Database connection settings
-            'DB_DRIVER' => 'mysql',
-            'DB_HOST' => 'srvr-sql.mysql.database.azure.com',
-            'DB_NAME' => 'imus_it_ticket_system',
-            'DB_USER' => 'admin1',
-            'DB_PASS' => 'root-123',
-            'DB_PORT' => 3306,
-            'SSL_MODE' => 'REQUIRED',
-            'MYSQL_ATTR_SSL_CA' => __DIR__ . '/config/DigiCertGlobalRootCA.crt.pem',
+            'API' => 'https://imus-it-ticket-system.azurewebsites.net/api/',
+            'API_KEY' => 'joshbakla',
+            'SITE_URL' => 'https://imus-it-ticket-system.azurewebsites.net/',
         ];
     } else {
         return [
-            // Database connection settings
-            'DB_DRIVER' => 'mysql',
-            'DB_HOST' => 'localhost',
-            'DB_NAME' => 'db_it_ticket_system',
-            'DB_USER' => 'root',
-            'DB_PASS' => 'password',
-            'DB_PORT' => 3306,
-            'SSL_MODE' => null,
-            'MYSQL_ATTR_SSL_CA' => null,
+            'API' => 'http://localhost:8080/api/',
+            'API_KEY' => 'joshbakla',
+            'SITE_URL' => 'http://localhost:8080/',
         ];
     }
 }
 
-foreach (defaultDatabaseConfig('azure') as $key => $value) {
+
+
+
+
+
+function defaultServer($default_server = 'local')
+{
+    if ($default_server == 'local') {
+        return [
+            'driver' => 'mysql',
+            'database' => 'db_it_ticket_system',
+            'host' => 'localhost',
+            'username' => 'root',
+            'password' => 'password',
+            'port' => '3306',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ];
+    } else if ($default_server == 'azure') {
+        return [
+            'driver' => 'mysql',
+            'database' => 'imus_it_ticket_system',
+            'host' => 'srvr-sql.mysql.database.azure.com',
+            'username' => 'admin1',
+            'password' => 'root-123',
+            'port' => '3306',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'sslmode'   => 'REQUIRED',
+            'options'   => [
+                PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/config/DigiCertGlobalRootCA.crt.pem'
+            ],
+        ];
+    }
+}
+
+define('CONFIG', defaultServer('azure'));
+
+foreach (defaultApiConfig() as $key => $value) {
     define($key, $value);
 }
